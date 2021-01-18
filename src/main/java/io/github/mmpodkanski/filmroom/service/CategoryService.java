033e90c443fc5fc055955a32578b22a7d9c01bb1
mmpod <mmpodkanski@gmail.com>
2021-01-18 09:52:31 +0100

    category management

--- src/main/java/io/github/mmpodkanski/filmroom/service/CategoryService.java
+++ src/main/java/io/github/mmpodkanski/filmroom/service/CategoryService.java
@@ -1,9 +1,12 @@
 package io.github.mmpodkanski.filmroom.service;
 
 import io.github.mmpodkanski.filmroom.models.Category;
-import io.github.mmpodkanski.filmroom.models.Movie;
+import io.github.mmpodkanski.filmroom.models.ECategory;
 import io.github.mmpodkanski.filmroom.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 class CategoryService {
@@ -13,15 +16,19 @@ class CategoryService {
         this.repository = repository;
     }
 
-    public Category returnExistsOrCreateNew(String categoryName, Movie movie) {
-        Category category = repository.findByName(categoryName)
-                .orElseGet(() -> {
-                    var newCategory = new Category(categoryName, false);
-                    return repository.save(newCategory);
-                });
+    public Set<Category> checkCategories(Set<String> categoriesToCheck) {
+        Set<Category> categories = new HashSet<>();
 
-        category.getMovies().add(movie);
-        return category;
+        if (categoriesToCheck.isEmpty()) {
+            throw new IllegalStateException("Categories are empty!");
+        }
 
+        categoriesToCheck.forEach(cat -> {
+            Category category = repository
+                    .findByName(ECategory.valueOf(cat))
+                    .orElseThrow(() -> new IllegalStateException("That category not exists!"));
+            categories.add(category);
+        });
+        return categories;
     }
 }
